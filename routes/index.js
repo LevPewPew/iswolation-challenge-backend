11f2ca33653f55af2c28488c703a9d8c30fd45be
LevPewPew <levthedev@protonmail.com>
2020-04-12 11:17:22 +1000

    cleanup

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const index = (req, res) => {
-  res.send('hello world');
-}
-
-router.get('/', index);
-
-// define routes here, this will mean anything in the categories-routes.js file (yet to be created) will assume that /categories will prepend the route path defined in categories-routes.js
-router.use('/categories', require('./categories-routes'));
 router.use('/games', require('./games-routes'));
 
 module.exports = router;
