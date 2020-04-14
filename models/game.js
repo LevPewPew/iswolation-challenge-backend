64f2d86edb7c529325375b7255df0ff57cf9bb8e
LevPewPew <levthedev@protonmail.com>
2020-04-14 19:42:06 +1000

    changed constants in game model to be more semantic

--- models/game.js
+++ models/game.js
@@ -5,12 +5,12 @@ const MIN_GROUP_NAME_CHARS = 1; // no blank names
 const MAX_GROUP_NAME_CHARS = 15; // 15 chosen to make frontend display logic easier
 const MIN_PLAYERS_CHARS = 1; // no blank names
 const MAX_PLAYERS_CHARS = 16; // 16 chosen to make frontend display logic easier
-const MIN_PLAYERS_COUNT = 1; // less than 1 player is pointless
-const MAX_PLAYERS_COUNT = 100; // 100, UX design decision
+const MIN_PLAYERS_LENGTH = 1; // less than 1 player is pointless
+const MAX_PLAYERS_LENGTH = 100; // 100, UX design decision
 const MIN_EXERCISE_NAME_CHARS = 1; // no blank names
 const MAX_EXERCISE_NAME_CHARS = 18;  // 18 chosen to make frontend display logic easier
-const MIN_EXERCISES_COUNT = 1; // less than 1 player is pointless
-const MAX_EXERCISES_COUNT = 10;  // 10, UX design decision
+const MIN_EXERCISES_LENGTH = 1; // less than 1 player is pointless
+const MAX_EXERCISES_LENGTH = 10;  // 10, UX design decision
 const MIN_REPS_COUNT = 1; // less than 1 rep is pointless
 const MAX_REPS_COUNT = 999; // 999 chosen to make frontend display logic easier, and also to prevent people from killing themselves reaching crazy reps
 
@@ -32,7 +32,7 @@ const gameSchema = new Schema(
           required: true
         }
       ],
-      validate: [playersCountLimit, `Must have between ${MIN_PLAYERS_COUNT} and ${MAX_PLAYERS_COUNT} players to begin`],
+      validate: [playersLengthLimit, `Must have between ${MIN_PLAYERS_LENGTH} and ${MAX_PLAYERS_LENGTH} players to begin`],
     },
     exercises: {
       type: [
@@ -52,7 +52,7 @@ const gameSchema = new Schema(
           }
         }
       ],
-      validate: [exercisesCountLimit, `Must have between ${MIN_PLAYERS_COUNT} and ${MAX_PLAYERS_COUNT} players to begin`],
+      validate: [exercisesLengthLimit, `Must have between ${MIN_PLAYERS_LENGTH} and ${MAX_PLAYERS_LENGTH} players to begin`],
     }
   },
   {
@@ -60,14 +60,14 @@ const gameSchema = new Schema(
   }
 );
 
-function playersCountLimit(value) {
-  const isWithinLimit = MIN_PLAYERS_COUNT <= value.length && value.length <= MAX_PLAYERS_COUNT;
+function playersLengthLimit(value) {
+  const isWithinLimit = MIN_PLAYERS_LENGTH <= value.length && value.length <= MAX_PLAYERS_LENGTH;
 
   return isWithinLimit;
 }
 
-function exercisesCountLimit(value) {
-  const isWithinLimit = MIN_EXERCISES_COUNT <= value.length && value.length <= MAX_EXERCISES_COUNT;
+function exercisesLengthLimit(value) {
+  const isWithinLimit = MIN_EXERCISES_LENGTH <= value.length && value.length <= MAX_EXERCISES_LENGTH;
 
   return isWithinLimit;
 }
